69dab8021d61e736a9f30fdd5badc7eb137c0ec1
Kelechi Daniels <dannykaylay@gmail.com>
2023-02-20 01:58:43 +0000

    adds method to password class to generate password

--- main.py
+++ main.py
@@ -8,16 +8,23 @@ def main():
     user_input = input(message)
 
     if user_input == "1":
-        password = Password(user_input)
 
-        use_for_password = password.special_characters + password.lower_case + password.upper_case_count + password.digits_count
-        password.password_length = 8
+        password_length = input("How many characters: \n")
 
-        generated_password = "".join(random.sample(use_for_password, password.password_length))
+        password = Password(user_input, password_length)
+
+        generated_password = password.generate_password()
     
         print(f"Your generated password is {generated_password}")
 
-    
+    # if user_input == "2":
+    #     password = Password(user_input)
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
--- password.py
+++ password.py
@@ -1,16 +1,29 @@
 
+import random as re
+
 class Password:
 
     special_characters_count = 0
     lower_case = 0
     upper_case_count= 0 
     digits_count = 0
-    password_length = 0
+
 
     special_characters = "@#$%&*/\?"
     lower_case = "abcdefghijklmnopqrstuvwxyz"
-    upper_case_count= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    digits_count = "1234567890"
+    upper_case= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    digits = "1234567890"
 
-    def __init__(self, password):
+
+    def __init__(self, password, password_length=8):
         self.password = password
+        self.password_length = int(password_length)
+        
+
+    def generate_password(self):
+
+        use_for_password = self.special_characters + self.lower_case + self.upper_case + self.digits
+
+        generated_password = "".join(re.sample(use_for_password, self.password_length))
+    
+        return generated_password
