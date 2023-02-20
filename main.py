155fa0c6025ecf678774b091c41c28b5cb1dd309
Kelechi Daniels <dannykaylay@gmail.com>
2023-02-20 17:29:30 +0000

    allows user to enter password multiple times

--- main.py
+++ main.py
@@ -5,25 +5,66 @@ def main():
     message = "Press 1 to auto-genetate a strong password\n"
     message += "Press 2 to enter a password you will like to validate its strength\n"
 
+    char_message = "How many characters (minimum 8 charaters): \n"
+    repeat = True
+
     user_input = input(message)
 
     if user_input == "1":
 
-        password_length = input("How many characters: \n")
+        while True:
 
-        password = Password(user_input, password_length)
+            password_length = input(char_message)
 
-        generated_password = password.generate_password()
-    
-        print(f"Your generated password is {generated_password}")
+            if int(password_length) >= 8:
 
-    # if user_input == "2":
-    #     password = Password(user_input)
+                password = Password(user_input, password_length)
+
+                generated_password = password.generate_password()
+
+                print(f"Your generated password is {generated_password}")
+
+                break
+
+            else:
+                print("Your password needs to be atleast 8 characters") 
 
 
 
+    if user_input == "2":
 
+        while repeat:
+            user_input = input("Enter your password below: \n")
 
+            if len(user_input) >= 8:
+                password = Password(user_input)
+
+                valid = password.validate_password()
+
+                if valid == True:
+                    print("Your password is strong!")
+                
+                else:
+                    print("Weak password! try a different combination for your password")
+
+            else:
+                print("Your password needs to be atleast 8 characters")
+            
+
+            repeat_message  = "Enter yes to try a new password or quit to end\n"
+
+            while repeat: 
+
+                repeat_message = input(repeat_message)
+
+                if repeat_message == "yes":
+                    break
+
+                elif repeat_message == "quit":
+                    repeat = False
+                
+                else:
+                    print("Wrong input!")
 
 
 if __name__ == "__main__":
--- password.py
+++ password.py
@@ -4,11 +4,11 @@ import random as re
 class Password:
 
     special_characters_count = 0
-    lower_case = 0
+    lower_case_count= 0
     upper_case_count= 0 
     digits_count = 0
-
-
+    valid = False
+    
     special_characters = "@#$%&*/\?"
     lower_case = "abcdefghijklmnopqrstuvwxyz"
     upper_case= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -21,9 +21,28 @@ class Password:
         
 
     def generate_password(self):
+        while True:
+            use_for_password = self.special_characters + self.lower_case + self.upper_case + self.digits
 
-        use_for_password = self.special_characters + self.lower_case + self.upper_case + self.digits
+            self.password = "".join(re.sample(use_for_password, self.password_length))
 
-        generated_password = "".join(re.sample(use_for_password, self.password_length))
+            if self.validate_password() == True:
+                return self.password
+
     
-        return generated_password
+    def validate_password(self):
+        for character in self.password:
+            if character in self.special_characters:
+                self.special_characters_count += 1
+            
+            if character in self.lower_case:
+                self.lower_case_count += 1
+
+            if character in self.upper_case:
+                self.upper_case_count += 1
+            
+            if character in self.digits:
+                self.digits_count += 1
+            
+        if self.special_characters_count > 0  and self.upper_case_count > 0 and self.lower_case_count > 0 and self.digits_count > 0:
+            return True
